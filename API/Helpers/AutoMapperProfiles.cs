647c8626e36b23670c19c853eb08994d7b5f7db8
Vinayak Jadhav <Vinayak.j@intelegain.com>
2021-09-01 17:42:27 +0530

    commit for section 9

---
+++ API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,23 @@
+using System.Linq;
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using AutoMapper;
+
+namespace API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        //Use of Auto Mapper install the Same from nuget pacakge
+        public AutoMapperProfiles()
+        {
+            CreateMap<AppUser,MemberDto>()
+            .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src=> 
+             src.Photos.FirstOrDefault(x=> x.IsMain).Url))
+             .ForMember(dest => dest.Age, opt => opt.MapFrom(
+                 src => src.DateOfBirth.CalculateAge()
+             ));
+            CreateMap<Photo,PhotoDto>();
+        }
+    }
+}
