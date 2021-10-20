427194956e45c97d8e0d67e97d0fbcf8563b6355
Vinayak Jadhav <Vinayak.j@intelegain.com>
2021-10-20 21:40:19 +0530

    commit for message changes

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -1,4 +1,5 @@
 using System.Linq;
+using API.Data;
 using API.DTOs;
 using API.Entities;
 using API.Extensions;
@@ -20,6 +21,11 @@ namespace API.Helpers
             CreateMap<Photo,PhotoDto>();
             CreateMap<MemberUpdateDto,AppUser>();
             CreateMap<RegisterDto,AppUser>();
+            CreateMap<Message, MessageDto>()
+              .ForMember(dest => dest.SenderPhotoUrl, opt => opt.
+                MapFrom(src => src.Sender.Photos.FirstOrDefault(x=> x.IsMain).Url))
+              .ForMember(dest => dest.RecipientPhotoUrl, opt => opt.
+                MapFrom(src => src.Recipient.Photos.FirstOrDefault(x=> x.IsMain).Url));
         }
     }
 }
---
+++ client/src/app/_models/message.ts
@@ -0,0 +1,12 @@
+export interface Message {
+    id: number;
+    senderId: number;
+    senderUsername: string;
+    senderPhotoUrl: string;
+    recipientId: number;
+    recipientUsername: string;
+    recipientPhotoUrl: string;
+    contet: string;1
+    dateRead?: Date;
+    messageSent: Date;
+  }
